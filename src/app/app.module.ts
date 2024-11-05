ff1ee1395707f649421b231c9e93279c792b7664
Dominik Nelic <dominik.nelic@gmail.com>
2024-11-05 08:54:06 +0100

    create heroNameModifier directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HeroesComponent } from './components/heroes/heroes.component';
+import { HeroNameModifierDirective } from './directives/hero-name-modifier.directive';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, DashboardComponent],
+  declarations: [AppComponent, HeroesComponent, DashboardComponent, HeroNameModifierDirective],
   imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/directives/hero-name-modifier.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+
+@Directive({
+  selector: '[appHeroNameModifier]',
+})
+export class HeroNameModifierDirective implements OnInit {
+  @Input('appHeroNameModifier') heroName: string = '';
+
+  constructor(private el: ElementRef) {}
+
+  ngOnInit(): void {
+    if (this.heroName.length > 10) {
+      this.el.nativeElement.textContent = `${this.heroName}_alza`;
+    } else {
+      this.el.nativeElement.textContent = this.heroName;
+    }
+  }
+}
